f99db2aa83ef6050c7b94230ece2a8ed7d071a33
HHasib <30808011+HHasib@users.noreply.github.com>
2018-09-06 17:35:32 +0600

    Add files via upload

---
+++ rest3.py
@@ -0,0 +1,180 @@
+from tkinter import*
+import random
+import time
+
+root = Tk()
+root.geometry("1600x700+0+0")
+root.title("MASTERMIND FOOD FEST")
+
+Tops = Frame(root,bg="white",width = 1600,height=50,relief=SUNKEN)
+Tops.pack(side=TOP)
+
+f1 = Frame(root,width = 900,height=700,relief=SUNKEN)
+f1.pack(side=LEFT)
+
+f2 = Frame(root ,width = 400,height=700,relief=SUNKEN)
+f2.pack(side=RIGHT)
+
+#------------------TIME--------------
+localtime=time.asctime(time.localtime(time.time()))
+#-----------------INFO TOP------------
+lblinfo = Label(Tops, font=( 'aria' ,30, 'bold' ),text="MASTERMIND FOOD FEST",fg="steel blue",bd=10,anchor='w')
+lblinfo.grid(row=0,column=0)
+lblinfo = Label(Tops, font=( 'aria' ,20, ),text=localtime,fg="steel blue",anchor=W)
+lblinfo.grid(row=1,column=0)
+
+
+status = Label(f2 ,font=('aria', 15, 'bold'),width = 20, text="-By Abu Hasnat Hasib",bd=2,relief=SUNKEN)
+
+status.grid(row=7,column=2)	
+
+
+text_Input=StringVar()
+operator =""
+
+
+
+def  btnclick(numbeTk):
+    global operator
+    operator=operator + str(numbeTk)
+    text_Input.set(operator)
+
+def clrdisplay():
+    global operator
+    operator=""
+    text_Input.set("")
+
+
+
+def qexit():
+    root.destroy()
+
+def reset():
+    rand.set("")
+    Fries.set("")
+    Largefries.set("")
+    Burger.set("")
+    Filet.set("")
+    Subtotal.set("")
+    Total.set("")
+    Service_Charge.set("")
+    Drinks.set("")
+    Tax.set("")
+    cost.set("")
+    Cheese_burger.set("")
+	
+def Ref():
+   
+#receives the total price
+    cof =float(Fries.get())
+    colfries= float(Largefries.get())
+    cob= float(Burger.get())
+    cofi= float(Filet.get())
+    cochee= float(Cheese_burger.get())
+    codr= float(Drinks.get())
+#change the price of meals here
+    costoffries = cof*25
+    costoflargefries = colfries*40 
+    costofburger = cob*35
+    costoffilet = cofi*50
+    costofcheeseburger = cochee*50
+    costofdrinks = codr*35
+#cost of total meal
+    costofmeal = "Tk.",str('%.2f'% (costoffries +  costoflargefries + costofburger + costoffilet + costofcheeseburger + costofdrinks))
+    
+    
+    Total.set(costofmeal)	
+	
+	
+#---------------------------------------------------------------------------------------
+
+Fries = StringVar()
+Largefries = StringVar()
+Burger = StringVar()
+Filet = StringVar()
+
+Total = StringVar()
+
+Drinks = StringVar()
+
+Cheese_burger = StringVar()
+
+
+
+
+lblfries = Label(f1, font=( 'aria' ,16, 'bold' ),text="Fries Meal",fg="steel blue",bd=10,anchor='w')
+lblfries.grid(row=1,column=0)
+txtfries = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Fries , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtfries.grid(row=1,column=1)
+
+lblLargefries = Label(f1, font=( 'aria' ,16, 'bold' ),text="Lunch Meal",fg="steel blue",bd=10,anchor='w')
+lblLargefries.grid(row=2,column=0)
+txtLargefries = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Largefries , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtLargefries.grid(row=2,column=1)
+
+
+lblburger = Label(f1, font=( 'aria' ,16, 'bold' ),text="Burger Meal",fg="steel blue",bd=10,anchor='w')
+lblburger.grid(row=3,column=0)
+txtburger = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Burger , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtburger.grid(row=3,column=1)
+
+lblFilet = Label(f1, font=( 'aria' ,16, 'bold' ),text="Pizza Meal",fg="steel blue",bd=10,anchor='w')
+lblFilet.grid(row=4,column=0)
+txtFilet = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Filet , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtFilet.grid(row=4,column=1)
+
+lblCheese_burger = Label(f1, font=( 'aria' ,16, 'bold' ),text="Cheese burger",fg="steel blue",bd=10,anchor='w')
+lblCheese_burger.grid(row=5,column=0)
+txtCheese_burger = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Cheese_burger , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtCheese_burger.grid(row=5,column=1)
+
+#--------------------------------------------------------------------------------------
+lblDrinks = Label(f1, font=( 'aria' ,16, 'bold' ),text="Drinks",fg="steel blue",bd=10,anchor='w')
+lblDrinks.grid(row=0,column=2)
+txtDrinks = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Drinks , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtDrinks.grid(row=0,column=3)
+
+
+
+lblTotal = Label(f1, font=( 'aria' ,16, 'bold' ),text="Total",fg="steel blue",bd=10,anchor='w')
+lblTotal.grid(row=5,column=2)
+txtTotal = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Total , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtTotal.grid(row=5,column=3)
+
+#-----------------------------------------buttons------------------------------------------
+lblTotal = Label(f1,text="---------------------",fg="white")
+lblTotal.grid(row=6,columnspan=3)
+
+btnTotal=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="TOTAL", bg="powder blue",command=Ref)
+btnTotal.grid(row=7, column=1)
+
+btnreset=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="RESET", bg="powder blue",command=reset)
+btnreset.grid(row=7, column=2)
+
+btnexit=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="EXIT", bg="powder blue",command=qexit)
+btnexit.grid(row=7, column=3)
+	
+	
+#---------------------------------------------------------------------------------------
+lblTotal = Label(f1,text="---------------------",fg="white")
+lblTotal.grid(row=6,columnspan=3)
+
+btnTotal=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="TOTAL", bg="powder blue",command=Ref)
+btnTotal.grid(row=7, column=1)
+
+btnreset=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="RESET", bg="powder blue",command=reset)
+btnreset.grid(row=7, column=2)
+
+btnexit=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="EXIT", bg="powder blue",command=qexit)
+btnexit.grid(row=7, column=3)
+
+	
+
+
+
+
+
+
+root.mainloop()
+	
+
