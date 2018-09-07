0f0c0daa5fb5ae3dde08ad8b0b65ea19e369ab0c
HHasib <30808011+HHasib@users.noreply.github.com>
2018-09-07 18:16:31 +0600

    Update main_program.py

--- main_program.py
+++ main_program.py
@@ -1,4 +1,5 @@
-from tkinter import* #imports all the functions from the tkinter library
+from tkinter import*
+import random
 import time
 
 root = Tk()
@@ -14,13 +15,11 @@ f1.pack(side=LEFT)
 f2 = Frame(root ,width = 400,height=700,relief=SUNKEN)
 f2.pack(side=RIGHT)
 
-#------------------TIME--------------
-localtime=time.asctime(time.localtime(time.time()))
+
 #-----------------INFO TOP------------
 lblinfo = Label(Tops, font=( 'aria' ,30, 'bold' ),text="MASTERMIND FOOD FEST",fg="steel blue",bd=10,anchor='w')
 lblinfo.grid(row=0,column=0)
-lblinfo = Label(Tops, font=( 'aria' ,20, ),text=localtime,fg="steel blue",anchor=W)
-lblinfo.grid(row=1,column=0)
+
 
 
 status = Label(f2 ,font=('aria', 15, 'bold'),width = 20, text="-By Abu Hasnat Hasib",bd=2,relief=SUNKEN)
@@ -28,20 +27,11 @@ status = Label(f2 ,font=('aria', 15, 'bold'),width = 20, text="-By Abu Hasnat Ha
 status.grid(row=7,column=2)	
 
 
-text_Input=StringVar()
+text_Input=IntVar()
 operator =""
 
 
 
-def  btnclick(numbeTk):
-    global operator
-    operator=operator + str(numbeTk)
-    text_Input.set(operator)
-
-def clrdisplay():
-    global operator
-    operator=""
-    text_Input.set("")
 
 
 
@@ -49,37 +39,44 @@ def qexit():
     root.destroy()
 
 def reset():
-    rand.set("")
-    Fries.set("")
-    Largefries.set("")
-    Burger.set("")
-    Filet.set("")
-    Subtotal.set("")
-    Total.set("")
-    Service_Charge.set("")
-    Drinks.set("")
-    Tax.set("")
-    cost.set("")
-    Cheese_burger.set("")
+    Pizza.set(0)
+    Sub.set(0)
+    Burger.set(0)
+    Donut.set(0)
+    Sumis.set(0)
+    Set_menu.set(0)
+    Chicken.set(0)
+    Drinks.set(0)
+    Cold_coffee.set(0)
+    Total.set(0)
+    
+  
 	
 def Ref():
    
 #receives the total price
-    cof =float(Fries.get())
-    colfries= float(Largefries.get())
-    cob= float(Burger.get())
-    cofi= float(Filet.get())
-    cochee= float(Cheese_burger.get())
+    copi =float(Pizza.get())
+    cosub= float(Sub.get())
+    cobu= float(Burger.get())
+    codo= float(Donut.get())
+    cosumi= float(Sumis.get())
+    coset= float(Set_menu.get())
+    coch= float(Chicken.get())
     codr= float(Drinks.get())
+    coco= float(Cold_coffee.get())
+	
 #change the price of meals here
-    costoffries = cof*25
-    costoflargefries = colfries*40 
-    costofburger = cob*35
-    costoffilet = cofi*50
-    costofcheeseburger = cochee*50
-    costofdrinks = codr*35
+    costofpizza = copi*150
+    costofsub =  cosub*150
+    costofburger = cobu*200
+    costofdonut = codo*120
+    costofsumis = cosumi*120
+    costofsetmenu = coset*300
+    costofchicken = coch*120
+    costofdrinks = codr*20
+    costofcoldcoffee = coco*80
 #cost of total meal
-    costofmeal = "Tk.",str('%.2f'% (costoffries +  costoflargefries + costofburger + costoffilet + costofcheeseburger + costofdrinks))
+    costofmeal = "Tk.",float('%.2f'% (costofpizza +  costofsub + costofburger + costofdonut + costofsumis + costofsetmenu + costofchicken + costofdrinks + costofcoldcoffee))
     
     
     Total.set(costofmeal)	
@@ -87,57 +84,74 @@ def Ref():
 	
 #---------------------------------------------------------------------------------------
 
-Fries = StringVar()
-Largefries = StringVar()
-Burger = StringVar()
-Filet = StringVar()
-
-Total = StringVar()
-
-Drinks = StringVar()
-
-Cheese_burger = StringVar()
-
-
+Pizza = IntVar()
+Sub = IntVar()
+Burger = IntVar()
+Donut = IntVar()
+Sumis = IntVar()
+Set_menu = IntVar()
+Chicken = IntVar()
+Drinks = IntVar()
+Cold_coffee = IntVar()
+Total = IntVar()
 
 
-lblfries = Label(f1, font=( 'aria' ,16, 'bold' ),text="Fries Meal",fg="steel blue",bd=10,anchor='w')
-lblfries.grid(row=1,column=0)
-txtfries = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Fries , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
-txtfries.grid(row=1,column=1)
 
-lblLargefries = Label(f1, font=( 'aria' ,16, 'bold' ),text="Lunch Meal",fg="steel blue",bd=10,anchor='w')
-lblLargefries.grid(row=2,column=0)
-txtLargefries = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Largefries , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
-txtLargefries.grid(row=2,column=1)
+lblPizza = Label(f1, font=( 'aria' ,16, 'bold' ),text="Pizza",fg="steel blue",bd=10,anchor='w')
+lblPizza.grid(row=1,column=0)
+txtPizza = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Pizza , bd=6,insertwidth=4,bg="powder blue" ,justify='left')
+txtPizza.grid(row=1,column=1)
+
+lblSub = Label(f1, font=( 'aria' ,16, 'bold' ),text="Sub",fg="steel blue",bd=10,anchor='w')
+lblSub.grid(row=2,column=0)
+txtSub = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Sub , bd=6,insertwidth=4,bg="powder blue" ,justify='left')
+txtSub.grid(row=2,column=1)
 
 
-lblburger = Label(f1, font=( 'aria' ,16, 'bold' ),text="Burger Meal",fg="steel blue",bd=10,anchor='w')
+lblburger = Label(f1, font=( 'aria' ,16, 'bold' ),text="Burger",fg="steel blue",bd=10,anchor='w')
 lblburger.grid(row=3,column=0)
-txtburger = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Burger , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtburger = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Burger , bd=6,insertwidth=4,bg="powder blue" ,justify='left')
 txtburger.grid(row=3,column=1)
 
-lblFilet = Label(f1, font=( 'aria' ,16, 'bold' ),text="Pizza Meal",fg="steel blue",bd=10,anchor='w')
-lblFilet.grid(row=4,column=0)
-txtFilet = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Filet , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
-txtFilet.grid(row=4,column=1)
+lblDonut = Label(f1, font=( 'aria' ,16, 'bold' ),text="Doughnut",fg="steel blue",bd=10,anchor='w')
+lblDonut.grid(row=4,column=0)
+txtDonut = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Donut , bd=6,insertwidth=4,bg="powder blue" ,justify='left')
+txtDonut.grid(row=4,column=1)
 
-lblCheese_burger = Label(f1, font=( 'aria' ,16, 'bold' ),text="Cheese burger",fg="steel blue",bd=10,anchor='w')
-lblCheese_burger.grid(row=5,column=0)
-txtCheese_burger = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Cheese_burger , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
-txtCheese_burger.grid(row=5,column=1)
+lblSumis = Label(f1, font=( 'aria' ,16, 'bold' ),text="Sumi's",fg="steel blue",bd=10,anchor='w')
+lblSumis.grid(row=5,column=0)
+txtSumis = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Sumis , bd=6,insertwidth=4,bg="powder blue" ,justify='left')
+txtSumis.grid(row=5,column=1)
+
+
+lblSet_menu = Label(f1, font=( 'aria' ,16, 'bold' ),text="Set_menu",fg="steel blue",bd=10,anchor='w')
+lblSet_menu.grid(row=1,column=2)
+txtSet_menu = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Set_menu , bd=6,insertwidth=4,bg="powder blue" ,justify='left')
+txtSet_menu.grid(row=1,column=3)
+
+lblChicken = Label(f1, font=( 'aria' ,16, 'bold' ),text="Chicken",fg="steel blue",bd=10,anchor='w')
+lblChicken.grid(row=2,column=2)
+txtChicken = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Chicken , bd=6,insertwidth=4,bg="powder blue" ,justify='left')
+txtChicken.grid(row=2,column=3)
+
+lblDrinks = Label(f1, font=( 'aria' ,16, 'bold' ),text="Drinks",fg="steel blue",bd=10,anchor='w')
+lblDrinks.grid(row=3,column=2)
+txtDrinks = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Drinks , bd=6,insertwidth=4,bg="powder blue" ,justify='left')
+txtDrinks.grid(row=3,column=3)
+
+
 
 #--------------------------------------------------------------------------------------
-lblDrinks = Label(f1, font=( 'aria' ,16, 'bold' ),text="Drinks",fg="steel blue",bd=10,anchor='w')
-lblDrinks.grid(row=0,column=2)
-txtDrinks = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Drinks , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
-txtDrinks.grid(row=0,column=3)
+lblCold_coffee = Label(f1, font=( 'aria' ,16, 'bold' ),text="Cold coffee",fg="steel blue",bd=10,anchor='w')
+lblCold_coffee.grid(row=4,column=2)
+txtCold_coffee = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Cold_coffee , bd=6,insertwidth=4,bg="powder blue" ,justify='left')
+txtCold_coffee.grid(row=4,column=3)
 
 
 
 lblTotal = Label(f1, font=( 'aria' ,16, 'bold' ),text="Total",fg="steel blue",bd=10,anchor='w')
 lblTotal.grid(row=5,column=2)
-txtTotal = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Total , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtTotal = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Total , bd=6,insertwidth=4,bg="powder blue" ,justify='left')
 txtTotal.grid(row=5,column=3)
 
 #-----------------------------------------buttons------------------------------------------
@@ -154,23 +168,9 @@ btnexit=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),wi
 btnexit.grid(row=7, column=3)
 	
 	
-#---------------------------------------------------------------------------------------
-lblTotal = Label(f1,text="---------------------",fg="white")
-lblTotal.grid(row=6,columnspan=3)
 
-btnTotal=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="TOTAL", bg="powder blue",command=Ref)
-btnTotal.grid(row=7, column=1)
-
-btnreset=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="RESET", bg="powder blue",command=reset)
-btnreset.grid(row=7, column=2)
-
-btnexit=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="EXIT", bg="powder blue",command=qexit)
-btnexit.grid(row=7, column=3)
 
 	
-
-
-
 
 
 
